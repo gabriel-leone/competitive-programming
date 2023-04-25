abac5df539d90cb0a8faef43f7237d63ed1f4245
gabriel-leone <110470412+gabriel-leone@users.noreply.github.com>
2023-04-25 20:23:47 -0300

    Create diferenca.cpp

---
+++ operation/diferenca.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,c,d;cin>>a>>b>>c>>d;
+    a=a*b;
+    c=c*d;
+    int diff = a-c;
+    cout << "DIFERENCA = " << diff << endl;
+}
